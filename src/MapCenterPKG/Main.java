19e234b448968d14e8eb41a71a44db009ea1cd0b
Mauro Merconchini <mauro_merconchini@yahoo.com>
2019-04-30 20:06:15 -0400

    Project initialization and first commit

---
+++ src/MapCenterPKG/Main.java
@@ -0,0 +1,26 @@
+package MapCenterPKG;
+
+import java.io.IOException;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+//        String exeDirectory = System.getProperty("user.dir");
+//        String workingDirectory = exeDirectory + "\\working";
+//        String brushShifterLocation = exeDirectory + "\\tools\\ReflexBrushShifter";
+//        String entityShifterLocation = exeDirectory + "\\tools\\ReflexEntityShifter";
+//
+//
+//        System.out.println("\nShifting Entities...");
+//
+//        Process myProcess = Runtime.getRuntime().exec(new String[]
+//                {
+//                    exeDirectory + "\\tools\\ReflexEntityShifter", exeDirectory + "\\cpm5.map", "10", "10", "10"
+//                });
+//
+//        System.out.println("Success\n");
+
+        
+    }
+}
